3eb6d7ac521088c61b3996852c96f9376c570fa2
Jonathan Érik <jonathan753_21@hotmail.com>
2022-11-15 18:02:50 -0300

    Finalização da atividade

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
+import React from 'react';
+import Imagem from './foto.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <img src={Imagem} className="foto" alt="Esquilo"/>
+      <h1><strong>Jonathan Érik Nascimento da Silva</strong></h1>
     </div>
   );
 }
